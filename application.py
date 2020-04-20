e84121ae7a95de6369b0edb1b4989043d8b8dabb
bhanuveda <bhanu110698@msitprogram.net>
2020-04-20 15:08:07 +0530

    Final Project-1

--- application.py
+++ application.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from models import *
+
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://fahyjekubolpsb:084f71e17a72960063160b48657280549094140468ebfe534b9b91b61101a54f@ec2-52-6-143-153.compute-1.amazonaws.com:5432/dacmhgi1g82sc0"
-
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+# setting up the secret key for the application
+app.config['SECRET_KEY'] = 'f9a1520561f1faf67f36a3a620a45e80'
+
 @app.route('/')
-@app.route('/register')
 def home():
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
     return render_template('register.html')
 
 @app.route('/result', methods=['GET','POST'])
@@ -31,8 +37,29 @@ def admin():
     print(data[0].username)
     return render_template('admin.html', data = data)
 
+@app.route('/login' , methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        user_name = request.form.get("user_name")
+        password = request.form.get("password_name")
+        print(user_name)
+        print(password)
+        user_obj = User.query.filter_by(username = user_name).first()
+        if password == user_obj.password and  user_name == user_obj.username:
+            session["USERNAME"] = user_name
+            return redirect(url_for("profile"))
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    if session.get("USERNAME") is not None:
+        return render_template('profile.html' , user_name = session.get("USERNAME"))
+    else:
+        return redirect(url_for("login"))    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
